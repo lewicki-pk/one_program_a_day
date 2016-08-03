059e81911f8ce0be5c029ac1374c3c1fe5095fc1
lewiatan <lewicki.pk@gmail.com>
2016-08-03 20:45:38 +0200

    2 python excercises on 03 Aug

---
+++ 03-aug_ex02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Question 2
+# Write a program which can compute the factorial of a given numbers.
+# 
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+def factorial(x):
+    if (x == 0) or (x == 1):
+        return 1
+    else:
+        return (x * factorial(x - 1))
+
+
+print("Insert the value for factorial:")
+variable = input()
+print(factorial(int(variable)))
